0cf167bcfcd9701ab9db40699e65cb94f17a10cf
enesbora <boraenes84@gmail.com>
2022-10-10 14:27:24 +0200

    more about variables

--- week4/monday/script.js
+++ week4/monday/script.js
@@ -2,4 +2,9 @@ var firstValue = 9;
 var myFirstSentence = "Hello World!";
 
 console.log(firstValue);
-console.log("My first print to console")
+console.log("My first print to console");
+
+let x = "This is a ";
+x + "string"; // add string to variable
+console.log(x);
+
