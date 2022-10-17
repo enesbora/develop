dce5c91951bb76e525406a0c29d4cdefd52da402
enesbora <boraenes84@gmail.com>
2022-10-17 15:54:04 +0200

    script.js is modified

--- week5/monday/which_number_is_bigger/script.js
+++ week5/monday/which_number_is_bigger/script.js
@@ -1,19 +1,19 @@
 document.getElementById("btn").addEventListener("click", btnOnclick);
 
 function btnOnclick(){
-  var num1 = parseInt(document.getElementById("num1").value);
-  var num2 = parseInt(document.getElementById("num2").value);
+  let num1 = parseInt(document.getElementById("num1").value);
+  let num2 = parseInt(document.getElementById("num2").value);
 
   if(isNaN(num1) || num1.length || isNaN(num1)){
       alert("Please write only numbers");
       return;
   }
 
-  let msg = document.getElementById("output");
+  let output = document.getElementById("output");
   if(num1 > num2)
-      msg.innerHTML = `The greater number of ${num1} and ${num2} is ${num1}.`;
+      output.innerHTML = `The greater number of ${num1} and ${num2} is ${num1}.`;
   else if(num1 < num2)
-      msg.innerHTML = `The greater number of ${num1} and ${num2} is ${num2}.`;
+      output.innerHTML = `The greater number of ${num1} and ${num2} is ${num2}.`;
   else
-      msg.innerHTML = "Numbers are equal.";
+      output.innerHTML = "Numbers are equal.";
 }
