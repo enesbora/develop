d1f968cf6dc9b5896bc029a8c9d8e2eade4a18df
enesbora <boraenes84@gmail.com>
2022-10-17 11:24:31 +0200

    gcd exercise

---
+++ week5/monday/gcd/gcd.js
@@ -0,0 +1,29 @@
+// START
+// 1) PROMPT "ENTER TWO POSITIVE NUMBERS"
+// 2) INITIALIZE three variable as m, n and r 
+// 3) FIND the bigger number
+// 4) SET the bigger number as m
+// 5) SET the smaller number as n
+// 6) DIVIDE m by n 
+// 7) IF (m%n = 0) == true => display “n is GCD”. END
+//    ELSE => r = m%n  
+// 	   SET m = n
+//                  SET n = r
+//                  AND GO TO step 6
+// EXAMPLE SOLUTION:
+// m:20 
+// n:12 
+// 20%12=8
+// r:8
+
+// m:12
+// n:8
+// 12%8=4
+// r:4
+
+// m:8
+// n:4
+// 8%4=0
+// r:0  
+
+// GCD = n = 4
