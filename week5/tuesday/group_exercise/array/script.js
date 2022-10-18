4de5d977109f13e5add85c2d8ad820d430d86d13
enesbora <boraenes84@gmail.com>
2022-10-18 11:21:48 +0200

    first exercise is is done

---
+++ week5/tuesday/group_exercise/array/script.js
@@ -0,0 +1,13 @@
+let array = ["Banana", "Apples", "Oranges", "Blueberries"];
+console.log(array[2]);
+
+array.shift();
+console.log(array);
+array.sort();
+console.log(array);
+array.push("Kiwi");
+console.log(array);
+array.shift();
+console.log(array);
+array.reverse();
+console.log(array);
