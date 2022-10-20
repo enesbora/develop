f549d977f0b21c09cba1169747c9cd479483ca11
enesbora <boraenes84@gmail.com>
2022-10-20 15:51:08 +0200

    bombs are made orange in minesweeper2

--- week5/thursday/minesweeper2/app.js
+++ week5/thursday/minesweeper2/app.js
@@ -9,16 +9,13 @@ document.addEventListener("DOMContentLoaded", () => {
     //get shuffled game array with random bombs
     const bombsArray = Array(bombAmounth).fill("bomb");
     const emptyArray = Array(width * width - bombAmounth).fill("valid");
-    console.log(bombsArray);
-    console.log(emptyArray);
     const gameArray = emptyArray.concat(bombsArray);
-    console.log(gameArray);
     const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
-    console.log(shuffledArray);
 
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement("div");
       square.setAttribute("id", i);
+      square.classList.add(shuffledArray[i]);
       grid.appendChild(square);
       squares.push(square);
     }
