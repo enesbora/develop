5ca8ad7fbd93c7b601077c1a06473da2aa70fd8d
enesbora <boraenes84@gmail.com>
2022-10-20 15:30:52 +0200

    bombs are randomly shuffled in minesweeper2

--- week5/thursday/minesweeper2/app.js
+++ week5/thursday/minesweeper2/app.js
@@ -1,10 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
   const grid = document.querySelector(".grid");
   let width = 10;
+  let bombAmounth = 20;
   let squares = [];
 
   //create Board
   function createBoard() {
+    //get shuffled game array with random bombs
+    const bombsArray = Array(bombAmounth).fill("bomb");
+    const emptyArray = Array(width * width - bombAmounth).fill("valid");
+    console.log(bombsArray);
+    console.log(emptyArray);
+    const gameArray = emptyArray.concat(bombsArray);
+    console.log(gameArray);
+    const shuffledArray = gameArray.sort(() => Math.random() - 0.5);
+    console.log(shuffledArray);
+
     for (let i = 0; i < width * width; i++) {
       const square = document.createElement("div");
       square.setAttribute("id", i);
