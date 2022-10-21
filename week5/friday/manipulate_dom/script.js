996e9f8139bfaece1f0f6dd10acbf72f8e47e1d0
enesbora <boraenes84@gmail.com>
2022-10-21 14:44:14 +0200

    exercise one

---
+++ week5/friday/manipulate_dom/script.js
@@ -0,0 +1,31 @@
+let header = document.getElementById("header");
+let prgrph = document.getElementById("prgrph");
+let anchor = document.getElementById("anchor");
+let aContent = "Anchor content";
+let prgrphContent = "Paragraph content";
+let headerContent = "content";
+
+function addHeaderContent() {
+  for (let i = 0; i < 2; i++) {
+    let text = `${headerContent} ${i + 1}`;
+    header.innerText += text;
+  }
+}
+addHeaderContent();
+
+function addParagraphContent() {
+  for (let i = 0; i < 4; i++) {
+    let text = `${prgrphContent} ${i + 1}`;
+    prgrph.innerText += text;
+  }
+}
+addParagraphContent();
+
+function anchorContent() {
+  for (let i = 0; i < 7; i++) {
+    let text = `${aContent} ${i + 1}`;
+    anchor.innerText += text;
+  }
+}
+
+anchorContent();
