007942801024e9d6a74e982cea75963b72380314
enesbora <boraenes84@gmail.com>
2022-10-21 11:15:24 +0200

    Dom manupulation exercise

---
+++ week5/friday/dom/script.js
@@ -0,0 +1,5 @@
+let header = document.createElement("h1");
+let node = document.createTextNode("Learning is great!");
+let parent = document.querySelector("#learnDom");
+header.appendChild(node);
+parent.appendChild(header);
