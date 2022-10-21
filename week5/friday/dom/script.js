194b80c628c57ebcc7077b8ba846b36f1b7f40e6
enesbora <boraenes84@gmail.com>
2022-10-21 11:39:31 +0200

    classlist toggling by clicking

--- week5/friday/dom/script.js
+++ week5/friday/dom/script.js
@@ -3,3 +3,9 @@ let node = document.createTextNode("Learning is great!");
 let parent = document.querySelector("#learnDom");
 header.appendChild(node);
 parent.appendChild(header);
+
+parent.addEventListener("click", myFunction);
+
+function myFunction() {
+  parent.classList.toggle("blue");
+}
