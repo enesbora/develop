f8ef5cbe9e0099850e070bf23c9ea07865545b73
enesbora <boraenes84@gmail.com>
2022-10-21 08:37:04 +0200

    last updates are saved

--- week5/thursday/minesweeper2/app.js
+++ week5/thursday/minesweeper2/app.js
@@ -19,6 +19,20 @@ document.addEventListener("DOMContentLoaded", () => {
       grid.appendChild(square);
       squares.push(square);
     }
+
+    //add numbers
+    for (let i = 0; i < squares.length; i++) {
+      let total = 0;
+      const isLeftEdge = i % width === 0;
+      const isRightEdge = i % width === -1;
+
+      if (squares[i].classList.contains("valid")) {
+        if (i > 0 && !isLeftEdge && squares[i - 1].classList.contains("bomb")) total++;
+        if (i > 9 && !isRightEdge && squares[i + 1 - width].classList.contains('bomb')) total++;
+        squares[i].setAttribute('data', total)
+        console.log(squares[i])
+      }
+    }
   }
   createBoard();
 });
