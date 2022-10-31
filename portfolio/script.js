3d73df09de4b1707c8267e9b82ac1ec07f08f6b6
enesbora <boraenes84@gmail.com>
2022-10-31 14:51:15 +0100

    menu toggling is done

--- portfolio/script.js
+++ portfolio/script.js
@@ -5,6 +5,8 @@ window.addEventListener("scroll", function () {
 
 function toggleMenu() {
   let menuToggle = document.querySelector(".toggle");
+  let menu = document.querySelector(".menu");
 
   menuToggle.classList.toggle("active");
+  menu.classList.toggle("active");
 }
