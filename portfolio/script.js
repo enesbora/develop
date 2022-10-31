4d1b32c96ba67698b226e8b097f65fa928150dd0
enesbora <boraenes84@gmail.com>
2022-10-31 14:19:19 +0100

    started to make the blog site responsive

--- portfolio/script.js
+++ portfolio/script.js
@@ -2,3 +2,9 @@ window.addEventListener("scroll", function () {
   let nav = document.querySelector("nav");
   nav.classList.toggle("sticky", window.scrollY > 0);
 });
+
+function toggleMenu() {
+  let menuToggle = document.querySelector(".toggle");
+
+  menuToggle.classList.toggle("active");
+}
